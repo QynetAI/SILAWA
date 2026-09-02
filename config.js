5e4d1b02b5e5604bb3d8d46ff6486bac442d582a
Qynet <airdropudien@gmail.com>
2026-09-02 19:46:07 +0700

    config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 tailwind.config = {
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-            },
+            fontFamily: { sans: ['Inter', 'sans-serif'] },
             colors: {
                 primary: '#2563eb',
                 secondary: '#16a34a',
-                accent: '#f59e0b',
+                accent: '#f59e0b'
             }
         }
     }
